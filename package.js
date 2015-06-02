c3b78c8ca0440323d6dbcfc2b5b7901ef26c22ff
Chris Mather <chris@eventedmind.com>
2015-06-01 17:01:40 -0700

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Dynamic layouts which enable rendering dynamic templates into regions on a page.',
-  version: '1.0.7',
+  version: '1.0.8',
   git: 'https://github.com/eventedmind/iron-layout'
 });
 
@@ -14,11 +14,11 @@ Package.on_use(function (api) {
   // some utils
   api.use('underscore');
 
-  api.use('iron:core@1.0.7');
+  api.use('iron:core@1.0.8');
   api.imply('iron:core');
 
   // dynamic templates
-  api.use('iron:dynamic-template@1.0.7');
+  api.use('iron:dynamic-template@1.0.8');
   api.imply('iron:dynamic-template');
 
   // if you use iron-layout you should get iron-dynamic-template for free!
